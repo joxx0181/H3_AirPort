e172184257a39f8cfdeb19709ce2a9f0b9fe7ee0
Jo-Anna Wahlstrøm Alfheim Sillemann <joxx0181@zbc.dk>
2021-08-25 23:51:34 +0200

    Add project files.

---
+++ H3_AirPort/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H3_AirPort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (AirPortDbEntities ctx = new AirPortDbEntities())
+            {
+                DbSet<Airline> airlinesQuery = ctx.Airline;
+
+                Console.WriteLine("Airports:");
+                Console.WriteLine("_________________");
+                foreach (AirportTerminal airport in ctx.AirportTerminal)
+                {
+                        Console.WriteLine("IATA: " + airport.IATA);
+                        Console.WriteLine("Airport: " + airport.AirportName);
+                        Console.WriteLine("Terminal:  " + airport.TerminalName);
+                        Console.WriteLine("Zipcode:  " + airport.ZipCode);
+
+                    Console.WriteLine();
+                }
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
